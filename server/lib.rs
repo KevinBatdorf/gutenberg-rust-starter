f93df4bac538225f61dffda28e8fd207951620f3
Kevin Batdorf <kbat82@gmail.com>
2022-02-28 01:35:13 -0500

    Remove extra space

--- server/lib.rs
+++ server/lib.rs
@@ -16,7 +16,7 @@ pub fn get_text() -> String {
         "\"Low-level programming is good for the programmer's soul.\" -John Carmack.",
         "\"Testing leads to failure, and failure leads to understanding.\" - Burt Rutan",
         "\"The only way to learn a new programming language is by writing programs in it.\" - Dennis Ritchie",
-        "\" Code is like humor. When you have to explain it, it's bad.\" - Cory House",
+        "\"Code is like humor. When you have to explain it, it's bad.\" - Cory House",
         "\"Controlling complexity is the essence of computer programming.\" - Brian Kernigan",
         "\"Don't worry if it doesn't work right.  If everything did, you'd be out of a job.\" - Mosher's Law of Software Engineering",
     ];
