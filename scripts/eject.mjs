7a07421a31f204e1493f320d05a19a078959e9ac
Kevin Batdorf <kbat82@gmail.com>
2022-03-27 10:38:25 -0400

    Add tw eject

--- scripts/eject.mjs
+++ scripts/eject.mjs
@@ -1,9 +1,10 @@
-// import fs from 'fs'
-// import { fileURLToPath } from 'url'
-// import { dirname } from 'path'
+import fs from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const __rootDir = dirname(__dirname)
 
 const command = process.argv[2]
 if (!command) {
@@ -14,5 +15,20 @@ if (!command) {
 console.log(`Running command: ${command}`)
 
 if (['tw', 'tailwind'].includes(command)) {
-    // Remove all Tailwind files and config
+    fs.unlinkSync('./tailwind.config.js')
+    fs.unlinkSync('./postcss.config.js')
+    const editorData = fs
+        .readFileSync(`${__rootDir}/src/editor/editor.css`, {
+            encoding: 'utf8',
+        })
+        .replace(/(@tailwind|--tw).*\n/g, '')
+    fs.writeFileSync(`${__rootDir}/src/editor/editor.css`, editorData)
+    const frontData = fs
+        .readFileSync(`${__rootDir}/src/front/style.css`, { encoding: 'utf8' })
+        .replace(/(@tailwind|--tw).*\n/g, '')
+    fs.writeFileSync(`${__rootDir}/src/front/style.css`, frontData)
+    console.log(
+        'Removed Tailwind files and config. Make sure to update your stylesheets accordingly.',
+    )
+    process.exit(0)
 }
