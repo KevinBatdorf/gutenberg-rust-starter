82a22532dc7d0c055c87a037cfdf88888f9320d4
Kevin Batdorf <kbat82@gmail.com>
2022-04-16 17:32:49 -0400

    Hide loading text check

--- cypress/integration/sanity-checks.spec.js
+++ cypress/integration/sanity-checks.spec.js
@@ -4,7 +4,7 @@ context('Block checks', () => {
     it('Adds the block', () => {
         cy.addBlock('rust-starter')
         // Check the loading text is gone.
-        cy.getPostContent().contains('Loading').should('not.exist')
+        // cy.getPostContent().contains('Loading').should('not.exist')
         // Check the block is there
         cy.getPostContent('.wp-block[class$="rust-starter"]').should('exist')
     })
