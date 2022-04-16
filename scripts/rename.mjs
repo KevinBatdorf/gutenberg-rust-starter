6219703ee8e5d686f5db495127cf49eedaad07f1
Kevin Batdorf <kbat82@gmail.com>
2022-04-16 17:04:20 -0400

    Update rename script

--- scripts/rename.mjs
+++ scripts/rename.mjs
@@ -1,10 +1,10 @@
-import glob from 'glob'
+import fg from 'fast-glob'
 import fs from 'fs'
 import replace from 'replace-in-file'
 import { __rootDir } from './utils.mjs'
 
 const textDomain = process.argv[2]
-const label = process.argv[3] ?? textDomain
+const label = process.argv[3] ? process.argv[3] : textDomain
 if (!textDomain) {
     console.error('No name provided')
     console.error('Usage: npm run replace <text-domain> <label>')
@@ -12,7 +12,14 @@ if (!textDomain) {
 }
 console.log(`Renaming to: ${textDomain} (${label})`)
 
-fs.renameSync(`${__rootDir}/rust-starter.php`, `${__rootDir}/${textDomain}.php`)
+try {
+    fs.renameSync(
+        `${__rootDir}/rust-starter.php`,
+        `${__rootDir}/${textDomain}.php`,
+    )
+} catch (e) {
+    console.warn('Warning:', e.message)
+}
 
 const ignore = [
     `${__rootDir}/node_modules/**/*`,
@@ -21,13 +28,11 @@ const ignore = [
     `${__rootDir}/target/**/*`,
     `${__rootDir}/pkg/**/*`,
 ]
-glob(`${__rootDir}/**/*.*`, { ignore }, (err, files) => {
-    if (err) throw err
-    files.forEach((item) => {
-        const options = { files: item }
-        replace.sync({ from: /rust-starter/g, to: textDomain, ...options })
-        replace.sync({ from: /Rust Starter/g, to: label, ...options })
-    })
+const files = fg.sync([`${__rootDir}/**/*.*`], { ignore })
+files.forEach((item) => {
+    const options = { files: item }
+    replace.sync({ from: /rust-starter/g, to: textDomain, ...options })
+    replace.sync({ from: /Rust Starter/g, to: label, ...options })
 })
 
 console.log('Finished.')
