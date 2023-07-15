28fc47c8a004783e3cdc49dd4942ce9000951805
Kevin Batdorf <kbat82@gmail.com>
2023-07-15 15:18:36 -0400

    Fix cli code

--- cypress/support/wp-cli.js
+++ cypress/support/wp-cli.js
@@ -1,9 +1,9 @@
 export const resetDatabase = () => cy.exec('wp-env clean all');
 export const installPlugin = (slug) =>
-	cy.exec(`wp-env run cli plugin install ${slug}`, {
+	cy.exec(`wp-env run cli wp plugin install ${slug} --activate`, {
 		failOnNonZeroExit: false,
 	});
 export const uninstallPlugin = (slug) =>
-	cy.exec(`wp-env run cli plugin uninstall ${slug}`, {
+	cy.exec(`wp-env run cli wp plugin uninstall ${slug}`, {
 		failOnNonZeroExit: false,
 	});
